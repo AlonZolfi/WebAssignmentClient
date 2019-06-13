a7bfd10ff0c5db1ca9829fea268922fa3eaedb15
hilakhadad <hilakhadad@gmail.com>
2019-06-13 16:35:05 +0300

    new 4 photos

--- public/pages/logIn/logInController.js
+++ public/pages/logIn/logInController.js
@@ -1,28 +1,6 @@
 angular.module("myApp")
     .controller("logInController", ['$scope','$http', '$location','$rootScope','$window',
         function ($scope,$http, $location, $rootScope, $window) {
-        $scope.logIn = function() {
-            var myObj = {username: $scope.user_name.valueOf(),password: $scope.user_pass.valueOf()};
-            var myJSON = JSON.stringify(myObj);
-            $http.post('http://localhost:3000/login', myJSON)
-                .then(function (response) {
-                    $rootScope.username = $scope.user_name.valueOf();
-                    $window.sessionStorage.setItem('token', response.data);
-                    alert("You are sing in.. just need to know were to move you");
-                    $location.path("/myAccount"); //need to be a "recommended for you"
-                })
-                .catch(function (error) {
-                    //console.log("onononon");
-                });
-        };
-        $scope.logOut = function(){
-            $rootScope.username = "Guest";
-            $window.sessionStorage.removeItem('token');
-        }
-    }]);
-angular.module("myApp")
-    .service("logout",['$scope','$http', '$location','$rootScope','$window',
-        function ($scope,$http, $location, $rootScope, $window) {
             $scope.logIn = function() {
                 var myObj = {username: $scope.user_name.valueOf(),password: $scope.user_pass.valueOf()};
                 var myJSON = JSON.stringify(myObj);
@@ -31,7 +9,7 @@ angular.module("myApp")
                         $rootScope.username = $scope.user_name.valueOf();
                         $window.sessionStorage.setItem('token', response.data);
                         alert("You are sing in.. just need to know were to move you");
-                        $location.path("/"); //need to be a "recommended for you"
+                        $location.path("/myAccount"); //need to be a "recommended for you"
                     })
                     .catch(function (error) {
                         //console.log("onononon");
@@ -40,6 +18,5 @@ angular.module("myApp")
             $scope.logOut = function(){
                 $rootScope.username = "Guest";
                 $window.sessionStorage.removeItem('token');
-                $location.path("/");
             }
         }]);
--- public/pages/myAccount/myAccountController.js
+++ public/pages/myAccount/myAccountController.js
@@ -1,5 +1,29 @@
 // poi controller
 angular.module("myApp")
-    .controller("myAccountController", function ($scope) {
+    .controller("myAccountController", [ '$scope','$http', '$location','$rootScope','$window',
+        function ($scope,$http, $location, $rootScope, $window) {
+            function getRecPOI() {
+                var myObj = {key: "x-auth-token",value: $window.sessionStorage.getItem("token") };
+                var myJSON = JSON.stringify(myObj);
+                $http.post('http://localhost:3000/recommendedPOI', myJSON)
+                    .then(function (response) {
+                        console.log("hbdfhdfhd");
+                    })
+                    .catch(function (error) {
+                        console.log("onononon");
+                    });
+            };
 
-    });
+            function getSavedPOI() {
+                var myObj = {key: "x-auth-token",value: $window.sessionStorage.getItem("token") };
+                var myJSON = JSON.stringify(myObj);
+                $http.post('http://localhost:3000/lastPOIsSaved', myJSON)
+                    .then(function (response) {
+                        console.log("hbdfhdfhd");
+                    })
+                    .catch(function (error) {
+                        console.log("onononon");
+                    });
+            };
+
+        }]);
