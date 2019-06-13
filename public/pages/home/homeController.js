02496a880e10f35d7f7203e95f9d2cfe748fc3dd
AlonZolfi <alonzolfi44@gmail.com>
2019-06-13 18:12:37 +0300

    added photos and fixed positioning

--- public/pages/home/homeController.js
+++ public/pages/home/homeController.js
@@ -1,7 +1,19 @@
 angular.module("myApp")
-    .controller("homeController", function(){
+    .controller("homeController", ['$http', function($http){
         var quadimages = document.querySelectorAll("#quad figure");
-        for(i=0; i<quadimages.length; i++) {
+        /*for(i=0; i<quadimages.length; i++) {
             quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") });
-        }
-    });
+        }*/
+        var minimalRank = 3;
+        $http.get('http://localhost:3000/randomPOI/'+minimalRank)
+            .then(function (response) {
+                var images = document.querySelectorAll("#quad img")
+                images[0].src = response.data[0].image;
+                images[1].src = response.data[1].image;
+                images[2].src = response.data[2].image;
+            })
+            .catch(function (error) {
+
+            });
+
+    }]);
