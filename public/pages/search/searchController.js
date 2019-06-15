008852f2c8e54a3c10f07b10977e05ba2f3572e5
AlonZolfi <alonzolfi44@gmail.com>
2019-06-15 18:09:02 +0300

    added sorting in search page

--- public/pages/search/searchController.js
+++ public/pages/search/searchController.js
@@ -7,4 +7,24 @@ angular.module("myApp")
             .catch(function (error) {
                 console.log("dsa");
             });
+        $scope.selected_sort = "";
+        $scope.sortBy = function () {
+            if ($scope.selected_sort === "Name") {
+                var compareName = function (poi1, poi2) {
+                    return poi1.name.localeCompare(poi2.name);
+                };
+                $scope.pois.sort(compareName);
+            }
+            else {
+                var compareRank = function (poi1, poi2) {
+                    if(poi1.rank < poi2.rank)
+                        return 1;
+                    else if (poi1.rank > poi2.rank)
+                        return -1;
+                    else
+                        return 0;
+                };
+                $scope.pois.sort(compareRank);
+            }
+        }
     }]);
